21589136d3f99caf0db05012f31a6f8d2c3d5150
nomaster <nomaster@chaosdorf.de>
2013-06-10 23:07:47 +0200

    gem update

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
-source :rubygems
+source "https://rubygems.org/"
 
 gem "middleman", "~>3.0.0.rc.3"
 gem "therubyracer"
 gem "bootstrap-sass"
+gem "middleman-livereload"
+gem "middleman-deploy"
--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
-### 
+###
 # Compass
 ###
 
 # Bootstrap
-# First: 
+# First:
 #    gem install sass-rails
 #    gem install bootstrap-sass
 require 'bootstrap-sass'
@@ -25,13 +25,13 @@ end
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -68,21 +68,29 @@ activate :directory_indexes
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
-  
+
   # Minify Javascript on build
   activate :minify_javascript
-  
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
   activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   set :http_path, "/img/"
+end
+
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host = "cepheus.uberspace.de"
+  deploy.path = "html"
+  deploy.clean = true
+  deploy.user = "ffddorf"
 end
